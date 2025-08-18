bc3b613457f5ffb445d0d9d8ccd4030eff5be434
Jika <jiksboy2000@gmail.com>
2025-08-18 14:14:40 +0100

    Add: hero background

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ export default function UniHelpLanding() {
       {/* Hero Section */}
       <section
         style={{
-          backgroundImage: `url(${backgroundImage})`,
+          backgroundImage: "/unihelp-background.jpeg",
           backgroundPosition: "center",
           backgroundSize: "contain",
           backgroundRepeat: "no-repeat",
